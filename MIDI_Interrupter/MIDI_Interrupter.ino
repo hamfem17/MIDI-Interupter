a08283bae28629e6fb711aa942067216971cdc39
marchersimon <marchersimon@zohomail.eu>
2021-03-01 14:50:06 +0100

    Almost Finish Header

--- MIDI_Interrupter/MIDI_Interrupter.ino
+++ MIDI_Interrupter/MIDI_Interrupter.ino
@@ -25,38 +25,58 @@ void setup() {
   
   for(int i = 0; i < 4; i++)
   {
-    buf[3 - i] = file.read(); 
-    Serial.println(buf[3 - i]);//header
+    buf[3 - i] = file.read();
+    //Serial.println(buf[3 - i],HEX);//header
   }
   
   if(*((unsigned long int *) buf) != 1297377380)
   {
-    Serial.println("Falsch");
+    Serial.println("Header invalid");
   }
   else
   {
-    Serial.println("Richtig");
+    Serial.println("Header correct");
   }
   
   for(int i = 0; i < 4; i++)
   {
     buf[3 - i] = file.read(); 
-    Serial.println(buf[3 - i]);//header lenght
+    //Serial.println(buf[3 - i],HEX);//header lenght
   }
   
   if(*((unsigned long int *) buf) != 6)
   {
-    Serial.println("Falsch");
+    Serial.println("Header lenght invalid");
   }
   else
   {
-    Serial.println("Richtig");
+    Serial.println("Header lenght correct");
+  }
+
+  for(int i = 0; i < 2; i++)
+  {
+    buf[1 - i] = file.read(); 
+    //Serial.println(buf[1 - i],HEX);// Format
+  }
+
+  switch(*((unsigned long int *) buf))
+  {
+    case 0: Serial.println("Single track file format not supported yet "); break; 
+    case 1: Serial.println("Format correct!"); break;
+    case 2: Serial.println("Multiple song file format not suported yet!"); break;
   }
   
-  for(int i = 5; file.available(); i++) {
+  for(int i = 0; i < 2; i++)
+  {
+    buf[1 - i] = file.read(); 
+    Serial.println(buf[1 - i],HEX);//number of track chunks that follow the header chunk 
+  }
+  
+  
+  for(int i = 9; i < 100; i++) {
     Serial.print(i);
     Serial.print(": ");
-    Serial.println((String)file.read());
+    Serial.println(file.read(),HEX);
   }
   file.close();
 }
