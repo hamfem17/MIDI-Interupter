652f875529394ee0d2fb3d6eb28347707044fe7f
hamfem17 <hamfem17@htl-kaindorf.at>
2021-03-03 10:18:38 +0100

    Finish Header

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,47 @@
 #include <SD.h>
 #include <SPI.h>
+#define PIN 10
 
 File file;
 
-long int readBytes(const int length) {
-    if(length > sizeof(long int)) {
-		return -1;
-	}
-
-	long int value = 0;
-    for(int i = 0; i < length; i++) {
-
-		long int hex = (file.read() << 8 * (length - i - 1));
-    Serial.println(hex,HEX);
-
-
-    long int k = 0x4D << 16;
-
-    if(k == 0)Serial.println("Hi");
-
-    Serial.println(k,HEX);
-    }
-	return value;
-}
-
-void setup() {
-
-  Serial.begin(9600);
+void initSD()
+{
   Serial.println("Initializing SD card");
 
-  if(!SD.begin(10)) { //PD4
+  if(!SD.begin(PIN))
+  { //PD4
       Serial.println("[Error]: Could not initialize SD card!");
       return;
   }
   
   Serial.println("Successfully initialized SD card");
 
-  if(!SD.exists("ACDC_TNT.MID")) {
+  if(!SD.exists("ACDC_TNT.MID"))
+  {
     Serial.println("File \"ACDC_TNT.MID\" not found");
   }
   
   file = SD.open("ACDC_TNT.MID");
+}
 
-  Serial.println(readBytes(4));
+long int readBytes(const int length)
+{
+    if(length > sizeof(long int)) {
+		return -1;
+	}
 
+	unsigned long int value = 0;
+    for(int i = 0; i < length; i++) {
+
+		value += ((unsigned long)file.read() << 8 * (length - i - 1));
+    
+    }
+
+	return value;
+}
+
+void checkHeader(unsigned int *trackChunks, unsigned int *division)
+{
   if(readBytes(4) != 1297377380) { //Check header string
     Serial.println("[Error]: Invalid header string");
   }
@@ -58,6 +56,7 @@ void setup() {
     Serial.println("Header lenght correct");
   }
 
+  
   switch(readBytes(2)) { //Check file format
     case 0: 
 		Serial.println("Single track file format not supported yet ");
@@ -73,12 +72,35 @@ void setup() {
   }
   
   Serial.print("Number of track chunks that follow the header chunk: "); //Read number of track chunks
-  Serial.println(readBytes(2));
+  *trackChunks = readBytes(2);
+  Serial.println(*trackChunks);
+
+  *division = readBytes(2);
+  if(*division >= 0x8000) { //Check division
+    Serial.println("SMPTE compatible units not supported yet");
+  }
+  else {
+    Serial.print("Speed = ");
+    Serial.print(*division);
+    Serial.println(" bpm");
+  }
+}
+
+void setup() {
+
+  unsigned int trackChunks;
+  unsigned int division;
+
+  Serial.begin(9600);
   
-  for(int i = 9; i < 100; i++) {
+  initSD();
+
+  checkHeader(&trackChunks, &division);
+
+  for(int i = 15; i < 100; i++) {
     Serial.print(i);
     Serial.print(": ");
-    Serial.println(file.read(),HEX);
+    Serial.println(file.read(),BIN);
   }
   file.close();
 }
